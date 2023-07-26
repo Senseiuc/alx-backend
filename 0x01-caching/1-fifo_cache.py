05dccd2572845e0ab66abe66a728ff9745cc4344
SenseiUC <62717218+Senseiuc@users.noreply.github.com>
2023-07-26 17:09:49 +0100

    fifo caching

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+A basic cache class that inherits from
+Base Caching
+Author:SenseiUC
+"""
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """
+    A class that implements first in first out out
+    approach of caching
+    """
+
+    def put(self, key, item):
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+            keys = self.cache_data.keys()
+            if len(keys) > BaseCaching.MAX_ITEMS:
+                print(f'DISCARD:{list(keys)[0]}')
+                del self.cache_data[list(keys)[0]]
+
+    def get(self, key):
+        """
+        returns an item using the key
+        """
+        if key is not None:
+            try:
+                return self.cache_data[key]
+            except KeyError:
+                return None
+        return None
